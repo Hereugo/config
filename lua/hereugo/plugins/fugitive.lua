a4d4285415a56a33e92e2c58aa9b50d01061dc8d
Amir Nurmukhambetov <thereug6@gmail.com>
2026-03-15 17:52:47 +0100

    Add descriptions to the key mapping code in fugitive.lua, explaining its functionality
    Signed-off-by: Amir Nurmukhambetov <thereug6@gmail.com>

--- lua/hereugo/plugins/fugitive.lua
+++ lua/hereugo/plugins/fugitive.lua
@@ -10,16 +10,20 @@ return {
 		vim.keymap.set("n", "<leader>gb", ":Git branch<Space>", { noremap = true, desc = "git branch" })
 		vim.keymap.set("n", "<leader>go", ":Git checkout<Space>", { noremap = true, desc = "git checkout" })
 
+		-- Map <leader>gc to perform a Git commit and interact with the Avante API
 		vim.keymap.set("n", "<leader>gc", function()
+			-- Trigger the Git commit command
 			vim.cmd("Git commit")
 
+			-- After a short delay, open a vertical split and interact with the Avante API
 			vim.defer_fn(function()
 				vim.cmd("vsplit")
+				-- Use the Avante API to ask a question related to the commit
 				require("avante.api").ask({
-					without_selection = true,
-					question = "/commit",
+					without_selection = true, -- No selection required
+					question = "/commit",    -- Specify the commit-related question
 				})
-			end, 100)
+			end, 100) -- Delay of 100ms before executing the function
 		end)
 	end,
 }
