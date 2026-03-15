0c0ecb6e109617a8293e4e6b1415c7694049e3bf
Amir Nurmukhambetov <thereug6@gmail.com>
2026-03-15 18:11:36 +0100

    Remove `--quiet` flag from Git push command.

--- lua/hereugo/plugins/fugitive.lua
+++ lua/hereugo/plugins/fugitive.lua
@@ -4,7 +4,7 @@ return {
 		vim.keymap.set("n", "<leader>gs", ":Git<CR>", { noremap = true, desc = "git status" })
 		vim.keymap.set("n", "<leader>ga", ":Git add ", { noremap = true, desc = "git add " })
 		vim.keymap.set("n", "<leader>gA", ":Git add .<CR>", { noremap = true, desc = "git add ." })
-		vim.keymap.set("n", "<leader>gp", ":Git push --quiet<CR>", { noremap = true, desc = "git push" })
+		vim.keymap.set("n", "<leader>gp", ":Git push <CR>", { noremap = true, desc = "git push" })
 		vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>", { noremap = true, desc = "git diff" })
 		vim.keymap.set("n", "<leader>gl", ":silent! GlLog<CR>", { noremap = true, desc = "git log" })
 		vim.keymap.set("n", "<leader>gb", ":Git branch<Space>", { noremap = true, desc = "git branch" })
@@ -32,6 +32,7 @@ return {
 
 							vim.defer_fn(function()
 								require("avante.api").ask({
+									new_chat = true,
 									without_selection = true,
 									question = "Write a concise git commit message for the staged changes. Return only the subject line.",
 								})
